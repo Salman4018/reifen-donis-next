eb2f07d3b681f9a456e1ef70e7c818c59b1293dc
Salman4018 <Salman4018@users.noreply.github.com>
2026-08-28 11:19:29 +0000

    deploy: 8f37f0592b031ac29fb1fe71f75966f9ed6483f3

---
+++ previews/feature-service-details/_next/static/wapZluHhhfa1-822SyB3f/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/reifen-donis-next/previews/feature-service-details//_next/:path+",
+        "destination": "/reifen-donis-next/previews/feature-service-details/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
