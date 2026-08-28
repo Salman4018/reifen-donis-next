2beb42db34bdd4f58f88c1021a46210dccfce53a
Salman4018 <Salman4018@users.noreply.github.com>
2026-08-28 14:29:34 +0000

    deploy: 270f4709fd969ef9d7a0ed2ceff0f9e57779813c

---
+++ previews/feature-modern-design/_next/static/hCZ2J8eZ1TudYtRuzil8D/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/reifen-donis-next/previews/feature-modern-design//_next/:path+",
+        "destination": "/reifen-donis-next/previews/feature-modern-design/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
